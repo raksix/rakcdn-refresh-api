37f127a75e0e7c9f4d285db7653d14f455ff81e9
Raksix <ermagfurkan7@gmail.com>
2024-12-10 14:25:37 +0300

    update

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.post('/upload', async (req, res) => {
 
    const mangaClient = new WebhookClient({ url: webhook });
 
-   buffer_images = JSON.parse(imagesdata)
+   buffer_images = imagesdata
 
    console.log(buffer_images)
    
